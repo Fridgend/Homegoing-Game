aa6459849e485552728391f96b2ece4f890e664a
Theodor Ristoff Nyberg <theodor.ristoffnyberg@gmail.com>
2026-02-28 19:39:49 +0100

    Reconfigured main loop for pygbag

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import pygame
+import asyncio
 
 from src.game import Game
 from src.game_backends.backend import GameState
 
-def main():
+async def main():
+    print("Hello, world, from async def main() #############################")
     pygame.init()
 
     state: GameState = GameState.MAIN_MENU
     game: Game = Game("assets/asset_guide.json", "scenes/scene_guide.json", "config.json", game_state=state)
-    game.run(60)
+    await game.run(60)
 
-if __name__ == "__main__":
-    main()
+asyncio.run(main())
